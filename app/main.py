51e7b6083b4c6154c5700913fbb897c1ec99fd30
hongyixiao <felixhong@live.com>
2019-10-07 18:07:09 -0400

    edited_v3

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ from app import webapp
 @webapp.route('/')
 def hello_world():
     return 'Hello World!' \
-           '            --- by 皮皮虾s:费利克斯大撒比 && giant handsome fredpan'
+           '            --- by 皮皮虾s: Awesome 费利克斯 && Awesome Giant Fredpan'
