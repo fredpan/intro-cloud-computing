788f6cd21824b053ba80226bb5f16e6fd7155536
fredpan <fredpan0821@gmail.com>
2019-10-07 15:53:29 -0400

    init
    Signed-off-by: fredpan <fredpan0821@gmaill.com>

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from app import webapp
+
+
+@webapp.route('/')
+def hello_world():
+    return 'Hello World!' \
+           '            --- by 皮皮虾s:费利克斯大撒比'
---
+++ run.py
@@ -0,0 +1,3 @@
+from app import webapp
+
+webapp.run("0.0.0.0", 5000)
