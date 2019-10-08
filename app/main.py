f8666cf532cb5686d6df069d7870d14e31864ca9
hongyixiao <felixhong@live.com>
2019-10-07 21:07:53 -0400

    changed port to localhost

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
+from flask import render_template
+
 from app import webapp
 
 
 @webapp.route('/')
 def hello_world():
-    pageContent = """
-    Hello World!
-            <div>--- by 皮皮虾s: Awesome 费利克斯 && Awesome Giant Fredpan</div>
-    """
-    return pageContent
+    return render_template("welcome.html")
