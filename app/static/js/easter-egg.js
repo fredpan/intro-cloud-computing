c6860fc3f3ec5e8f4df56eb4108dd03688ec4929
felixhongyx <felixhong@gmail.com>
2019-10-08 17:08:00 -0400

    XIAOLAODI

--- app/static/js/easter-egg.js
+++ app/static/js/easter-egg.js
@@ -1,17 +1,17 @@
 var q = [];
-q[0]="Yo?";
-q[1]="What's up?";
-q[2]="Hi Boss?";
-q[3]="Yes Boss?";
-q[4]="Yes?";
-q[5]="How are you today, boss?";
+q[0]="SUBMIT HERETIC!";
+q[1]="EMBRACE THE ELDER GOD!";
+q[2]="THERE IS NO ESCAPE!";
+q[3]="EXISTENCE IS PAIN!";
+q[4]="JOIN US!";
+q[5]="THIS WORLD IS AN ILLUSION!";
 
 function wowwowwowwowwowwowwowwowwow(){
     var c = q.length;
     var i = Math.floor(Math.random() * c);
     var aws = confirm(q[i]);
     console.log(aws);
-    while (aws){
+    while (true){
         i = Math.floor(Math.random() * c);
         aws = confirm(q[i]);
     }
